265c8169e2ac486a3c17e34a60b3c35d5044eb7f
Elelero <91528640+Elelero@users.noreply.github.com>
2022-09-04 21:14:50 +0900

    프로그래머스_로또 최고 순위와 최저순위

--- codingtest/Problem11.java
+++ codingtest/Problem11.java
@@ -4,32 +4,33 @@ import java.util.Arrays; //배열 정렬을 위해 쓴 배열 라이브러리
 import java.util.Scanner;
 
 public class Problem11 {
-	public static void main(String[] args) {
+	
+    public static void main(String[] args) {
 		
-		//#문제(Lv01) 로또 최고 순위와 최저순위
-		//배열
-		int[] lottos = {44, 1, 0, 0, 31, 25};
-		int[] win_nums = {31, 10, 45, 1, 6, 19};
+	//#문제(Lv01) 로또 최고 순위와 최저순위
+	//배열
+	int[] lottos = {44, 1, 0, 0, 31, 25};
+	int[] win_nums = {31, 10, 45, 1, 6, 19};
 
-		//변수
+	//변수
         int min = 0; //일치하는 번호의 최소개수
         int max = 0; //일치하는 번호의 최대개수
         
         //반복문
         for(int i=0; i<6; i++){
-        	for(int j=0; j<6; j++){
-        		//최소개수
-        		if(lottos[i] != 0){
-                    if(lottos[i]==win_nums[j]){
-                        min++;
-                    }
-        		}
-        	}
-        	//0에 들어갈 수 있는 최대개수
-    		if(lottos[i]==0) {
-    			max++;
-        		}
-        	}
+	    for(int j=0; j<6; j++){
+		//최소개수
+		if(lottos[i] != 0){
+		    if(lottos[i]==win_nums[j]){
+			min++;
+		    }
+	        }
+	    }
+	    //0에 들어갈 수 있는 최대개수
+	    if(lottos[i]==0) {
+		max++;
+	    }
+	}
         
         //최종 최대개수
         max = min + max;
@@ -51,5 +52,5 @@ public class Problem11 {
         	System.out.println(answer[i]);
         }		
 		
-	}
+    }
 }
