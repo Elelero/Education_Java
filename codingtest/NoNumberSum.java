8a80a51a47aaf043162b046115185e588b8fc33c
Elelero <91528640+Elelero@users.noreply.github.com>
2022-09-05 20:15:16 +0900

    프로그래머스_없는숫자더하기

--- codingtest/NoNumberSum.java
+++ codingtest/NoNumberSum.java
@@ -3,22 +3,22 @@ package codingTest;
 public class NoNumberSum {
 
 	public static int main(int[] numbers) {
-		        // int answer = -1;
-		        
-		        int sum1 = 0;
-		        //1~9까지 더하기
-		        for(int i=1; i<=9; i++){
-		            sum1 += i;
-		        }
-		        
-		        int sum2 = 0;
-		        //배열 수 더하기
-		        for(int i=0; i<numbers.length; i++){
-		            sum2 += numbers[i];
-		        }
-		        
-		        int answer = sum1 - sum2;
-		        return answer;
+		// int answer = -1;
+
+		int sum1 = 0;
+		//1~9까지 더하기
+		for(int i=1; i<=9; i++){
+		    sum1 += i;
+		}
+
+		int sum2 = 0;
+		//배열 수 더하기
+		for(int i=0; i<numbers.length; i++){
+		    sum2 += numbers[i];
+		}
+
+		int answer = sum1 - sum2;
+		return answer;
 		        
 	}
 
