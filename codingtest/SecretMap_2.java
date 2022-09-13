ebc386ac0e71388b196d1b7cc7a8c6c1a4286e8e
Elelero <91528640+Elelero@users.noreply.github.com>
2022-09-13 20:24:04 +0900

    프로그래머스_비밀지도_AnotherSolution

--- codingtest/SecretMap_2.java
+++ codingtest/SecretMap_2.java
@@ -1,35 +1,35 @@
 class Solution {
     public String[] solution(int n, int[] arr1, int[] arr2) {
         int[] number1Arr = new int[n];
-		int[] number2Arr = new int[n];
-		int[] reverse = new int[n];
-		int[] reverse2 = new int[n];
-		String[] output = new String[n];
-		
-		for(int i=0; i<n; i++) {
-			for(int j=0; j<n; j++) {
-				number1Arr[j] = arr1[i] % 2;
-				arr1[i] = arr1[i] / 2;
-				number2Arr[j] = arr2[i] % 2;
-				arr2[i] = arr2[i] / 2;
-			}
-			int a = 0;
-			for(int j=n-1; j>=0; j--) {
-				reverse[a] = number1Arr[j];
-				reverse2[a] = number2Arr[j];
-				a++;
-			}
-			String finish = "";
-			for(int j=0; j<n; j++) {
-				if(reverse[j] + reverse2[j] > 0) {
-					finish += "#";
-				} else {
-					finish += " ";
-				}
-			}
-			output[i] = finish;
+	int[] number2Arr = new int[n];
+	int[] reverse = new int[n];
+	int[] reverse2 = new int[n];
+	String[] output = new String[n];
+
+	for(int i=0; i<n; i++) {
+		for(int j=0; j<n; j++) {
+			number1Arr[j] = arr1[i] % 2;
+			arr1[i] = arr1[i] / 2;
+			number2Arr[j] = arr2[i] % 2;
+			arr2[i] = arr2[i] / 2;
 		}
-        String[] answer = output;
-        return answer;
+		int a = 0;
+		for(int j=n-1; j>=0; j--) {
+			reverse[a] = number1Arr[j];
+			reverse2[a] = number2Arr[j];
+			a++;
+		}
+		String finish = "";
+		for(int j=0; j<n; j++) {
+			if(reverse[j] + reverse2[j] > 0) {
+				finish += "#";
+			} else {
+				finish += " ";
+			}
+		}
+		output[i] = finish;
+	}
+String[] answer = output;
+return answer;
     }
 }
