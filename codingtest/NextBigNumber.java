e94c7d81abf79060b602c38ee295874207f6e23b
Elelero <91528640+Elelero@users.noreply.github.com>
2022-09-26 22:25:36 +0900

    프로그래머스_다음 큰 숫자

--- codingtest/NextBigNumber.java
+++ codingtest/NextBigNumber.java
@@ -37,3 +37,36 @@ class Solution {
         
     }
 }
+
+
+//다른 풀이
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        int count1 = 0;
+        int count2 = 0;
+        
+        count1 = countOne(n, 0);
+
+        while(true) {
+        	n++;
+        	count2 = countOne(n, 0);
+        	if(count1==count2) {
+        		break;
+        	}
+        }
+        return n;
+    }
+    
+    public static int countOne(int n, int count){
+    	String binaryStr = Integer.toBinaryString(n);
+        String[] binaryArr = binaryStr.split("");
+        for(int i=0; i<binaryArr.length; i++) {
+            if(binaryArr[i].equals("1")) {
+                count++;
+            }
+        }
+        return count;
+    }
+    
+}
