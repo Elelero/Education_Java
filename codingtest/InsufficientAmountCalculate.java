4aa60e9c80da4b1ad67cd55d3620e21cf1f8d0a8
Elelero <91528640+Elelero@users.noreply.github.com>
2022-09-29 23:14:47 +0900

    프로그래머스_부족한 금액 계산하기

---
+++ codingtest/InsufficientAmountCalculate.java
@@ -0,0 +1,15 @@
+class Solution {
+    public long solution(int price, int money, int count) {
+        long answer = 0;
+
+        for(int i=1; i<=count; i++){
+            answer += (price*i);
+            if(i==count && answer>money){
+                answer -= money;
+            } else if(i==count && answer<=money) {
+                answer = 0;
+            }
+        }
+        return answer;
+    }
+}
