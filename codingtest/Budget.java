fc0fa9d82a01f14f61fef63a383a0276f75f42ba
Elelero <91528640+Elelero@users.noreply.github.com>
2022-10-05 19:36:47 +0900

    프로그래머스_예산

--- codingtest/Budget.java
+++ codingtest/Budget.java
@@ -39,3 +39,22 @@ class Solution {
         return answer;
     }
 }
+
+
+//다른 풀이------------------------------------------
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int[] d, int budget) {
+        int sum=0;
+        int count=0;
+        Arrays.sort(d);
+        for(int j : d) {
+           sum+=j;
+           if(sum<=budget) {
+              count++;
+           }
+        }
+        return count;
+    }
+}
