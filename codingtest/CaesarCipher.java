23bdb9c1539d204d8653701145fbacb51516a796
Elelero <91528640+Elelero@users.noreply.github.com>
2022-10-07 10:49:22 +0900

    프로그래머스_시저암호

--- codingtest/CaesarCipher.java
+++ codingtest/CaesarCipher.java
@@ -23,3 +23,51 @@ class Solution {
         return answer;
     }
 }
+
+//다른풀이------------------------------------------------------------------------
+class Solution {
+    public String solution(String s, int n) {
+        char[] arr = new char[s.length()];
+        String answer = "";
+        for(int i=0; i<arr.length; i++) {
+            arr[i] = s.charAt(i);
+            if('a' <= arr[i] && arr[i] <= 'z') {
+                arr[i] += n;
+                if(arr[i] > 'z') {
+                    arr[i] -= 26;
+                }
+            } else if ('A' <= arr[i] && arr[i] <= 'Z') {
+                arr[i] += n;
+                if(arr[i] > 'Z') {
+                    arr[i] -= 26;
+                }
+            }
+            answer += arr[i];
+        }
+        return answer;
+    }
+}
+
+//다른풀이------------------------------------------------------------------------
+class Solution {
+    public String solution(String s, int n) {
+      String answer = "";
+      for (int i = 0; i < s.length(); i++) {
+         char alpa = s.charAt(i);
+
+         if (alpa >= 'a' && alpa <= 'z') { // 소문자
+            if (alpa + n > 'z') // z가 있을 때,
+               answer += (char) (alpa + n - 26);
+            else
+               answer += (char) (alpa + n);
+         } else if (alpa >= 'A' && alpa <= 'Z') { // 대문자
+            if (alpa + n > 'Z') // Z가 있을 때,
+               answer += (char) (alpa + n - 26);
+            else
+               answer += (char) (alpa + n);
+         } else
+            answer += (char) alpa;
+      }
+      return answer;
+    }
+}
